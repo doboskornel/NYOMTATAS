c445f4f7813a3f19aaae65625f21a001f31a6568
doboskornel <70878431+doboskornel@users.noreply.github.com>
2021-03-30 20:21:48 +0200

    Add files via upload

---
+++ NYOMTATAS/nyomtatas.js
@@ -0,0 +1,16 @@
+function kalkulal(){
+    //Űrlapadatok
+    const szelesseg=document.getElementById("szelesseg").value;
+    const magassag=document.getElementById("magassag").value;
+    const papir=document.getElementById('papirtipus').value;
+    //Számítások
+    let terulet=Math.round((szelesseg*magassag)/10000);    
+    let koltseg=terulet*papir;
+    //Megjelenítés
+
+    document.getElementById("terulet").textContent = terulet; 
+    document.getElementById("papir").textContent = papir; 
+    document.getElementById("koltseg").textContent = koltseg; 
+    document.getElementById('valasz').style.visibility = "visible";
+}
+
